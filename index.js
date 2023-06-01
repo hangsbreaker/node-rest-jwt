b647967c476a01ffbca62609ee2fc2fddc5f18ed
Hangs Breaker <hangsbreaker@gmail.com>
2023-06-01 09:52:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,18 @@ const authRoutes = require("./routes/auth");
 app.use("/auth", authRoutes);
 // end routes ========
 
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+
 app.listen(port, () => {
   console.log(`App running at port ${port}`);
 });
