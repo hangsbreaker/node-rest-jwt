b848afb9323b9252a34cf3b36354118c4c597da9
Hangs Breaker <hangsbreaker@gmail.com>
2023-06-01 08:35:12 +0700

    Update includes.js

--- includes.js
+++ includes.js
@@ -3,4 +3,4 @@ const router = express.Router();
 const verify = require("./verify");
 const db = require("./con");
 
-module.exports = { db, router, verify };
+module.exports = { router, verify, db };
